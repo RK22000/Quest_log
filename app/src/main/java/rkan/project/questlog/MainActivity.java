afcf01381de449cea89938bd437be2ac756e7e3d
Rahul <rahukand@gmail.com>
2021-08-02 18:17:16 -0700

    Quest button layout visibility
    It now depends on wether there is text in the text input.

--- app/src/main/java/rkan/project/questlog/MainActivity.java
+++ app/src/main/java/rkan/project/questlog/MainActivity.java
@@ -3,6 +3,13 @@ package rkan.project.questlog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.EditText;
+
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextInputEditText inputEditText = findViewById(R.id.questInputText);
+        inputEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                View layout = findViewById(R.id.questButtonLayout);
+                if (s.length() == 0) {
+                    layout.setVisibility(View.GONE);
+                } else {
+                    layout.setVisibility(View.VISIBLE);
+                }
+
+            }
+        });
     }
 }
