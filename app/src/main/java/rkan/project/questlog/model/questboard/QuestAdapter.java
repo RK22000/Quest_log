67596b9e3335f9437b8d91ff5077cd23c0e3ccc1
Rahul <rahukand@gmail.com>
2022-01-02 14:01:07 -0800

    Quests drop to the bottom of the list when they are completed

--- app/src/main/java/rkan/project/questlog/model/questboard/QuestAdapter.java
+++ app/src/main/java/rkan/project/questlog/model/questboard/QuestAdapter.java
@@ -6,6 +6,7 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import rkan.project.questlog.model.Quest;
@@ -37,7 +38,19 @@ class QuestAdapter extends RecyclerView.Adapter<QuestViewHolder>{
     @Override
     public void onBindViewHolder(@NonNull QuestViewHolder holder, int position) {
         holder.setQuest(quests.get(position), updatedQuests -> {
-            notifyItemChanged(position);
+            int pos = holder.getAdapterPosition();
+            notifyItemChanged(pos);
+            // if only one quest updated
+            if (updatedQuests.length == 1) {
+                Quest quest = updatedQuests[0];
+                // if that quest is now completed
+                if (quest.completed) {
+                    // move it to the bottom
+                    quest.weight = getQuests().get(getItemCount()-1).weight+1;
+                    Collections.sort(quests, (q1,q2)->q1.weight-q2.weight);
+                    notifyItemMoved(pos , getItemCount()-1);
+                }
+            }
             questUpdateCallback.call(updatedQuests);
         });
     }
